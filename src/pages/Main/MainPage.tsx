16c418ce1b095d2b5f37410db17b13a53991c6ea
DGUHJH <ghkdwogud852@gmail.com>
2023-03-27 20:18:53 +0900

    style : MainPage 레이아웃 수정

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { usePostTtsMutation } from 'api/postTts'
+import { FC, useEffect } from 'react'
 import { Root } from './styled'
 
 type MainPageProps = {
@@ -6,5 +7,11 @@ type MainPageProps = {
 }
 
 export const MainPage: FC<MainPageProps> = ({ className }) => {
+  const { mutate: postTtsMutate } = usePostTtsMutation()
+
+  useEffect(() => {
+    postTtsMutate({ speaker: '0', text: '안녕하세요' })
+  }, [])
+
   return <Root className={className}>test</Root>
 }
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,3 +1,3 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const Root = styled.div``;
+export const Root = styled.div``
